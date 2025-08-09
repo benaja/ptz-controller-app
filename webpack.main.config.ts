8406e51c1405c7e170096ce3137671bf02e6b356
Benaja <benhu00@outlook.com>
2025-08-09 15:20:27 +0200

    move rules to main

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -20,7 +20,19 @@ export const mainConfig: Configuration = {
   ],
 
   module: {
-    rules,
+    rules: [
+      ...rules,
+      {
+        test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+          loader: '@vercel/webpack-asset-relocator-loader',
+          options: {
+            outputAssetBase: 'native_modules',
+          },
+        },
+      },
+    ],
   },
   plugins,
   resolve: {
