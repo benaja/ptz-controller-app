c679fa5ef68ed3af2921850384dc8d9c8849df11
Benaja <benhu00@outlook.com>
2025-08-09 15:14:23 +0200

    try to fix

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -3,12 +3,12 @@ import path from 'path';
 
 export const rules: Required<ModuleOptions>['rules'] = [
   // Add support for native node modules
-  // {
-  //   // We're specifying native_modules in the test because the asset relocator loader generates a
-  //   // "fake" .node file which is really a cjs file.
-  //   test: /native_modules[/\\].+\.node$/,
-  //   use: 'node-loader',
-  // },
+  {
+    // We're specifying native_modules in the test because the asset relocator loader generates a
+    // "fake" .node file which is really a cjs file.
+    test: /native_modules[/\\].+\.node$/,
+    use: 'node-loader',
+  },
   // {
   //   test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
   //   parser: { amd: false },
